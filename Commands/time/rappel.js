a9530f24078b98ca5287bd5601777e36a3cbaa16
DaemonWhite <matheo.daemon@gmail.com>
2023-12-15 21:24:24 +0100

    add rappel

--- Commands/time/rappel.js
+++ Commands/time/rappel.js
@@ -1,19 +1,32 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+} 
+  
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rappel')
 		.setDescription('rappel au bout de x temps!')
-		.addStringOption(option =>
-			option.setName('temps')
+		.addIntegerOption(option =>
+			option
+                .setName('minute')
 				.setDescription('The input to echo back')
 				.setRequired(true))
         .addStringOption(option =>
             option
                 .setName("message")
+                .setDescription("Message a mettre")
                 .setRequired(false)),
 	async execute(interaction) {
+        message = interaction.options.getString("message")
+        time = interaction.options.getInteger("minute")
+
+        await interaction.reply(`message dans ${time} minute`)
+
+        await delay(time * 60000)
         
-		await interaction.reply('Pong!');
+		await interaction.channel.send(message);
 	},
 };
