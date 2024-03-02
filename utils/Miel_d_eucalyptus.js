4e88701a095eeaf56a1796d3c035f508f3bac3f6
LoevanCailleaud <cailleaudloevan@gmail.com>
2024-03-02 20:36:02 +0100

    update

--- utils/Miel_d_eucalyptus.js
+++ utils/Miel_d_eucalyptus.js
@@ -9,4 +9,38 @@ class reaction_v2 {
         this._emoji = emoji
     }
     
+    get_role() {
+        return role;
+    }
+
+    get_description() {
+        return description;
+    }
+
+    get_emoji() {
+        return emoji;
+    }
+}
+
+class gestion_reaction {
+    tab_reaction = []
+    longeur = 0
+    ajouter_reaction(role, description, emoji) {
+        let reaction = new reaction_v2(role, description, emoji)
+        this.tab_reaction.push(reaction)
+        this.longeur ++
+    }
+
+    supprimer_reaction() {
+        this.tab_reaction = []
+        this.longeur = 0
+    }
+
+    recuperer_reaction(index) {
+        return this.tab_reaction[index]
+    }
+
+    recuperer_longueur() {
+        return this.longeur
+    }
 }
