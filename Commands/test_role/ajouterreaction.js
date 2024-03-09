a63450fd0ff5d9f82fb2f68290b5da96e5aaedf2
LoevanCailleaud <cailleaudloevan@gmail.com>
2024-03-09 21:31:12 +0100

    update

--- Commands/test_role/ajouterreaction.js
+++ Commands/test_role/ajouterreaction.js
@@ -19,23 +19,32 @@ module.exports = {
             msg.addFields({name:' ', value: `${react.get_role()} ${react.get_description()} ${react.get_emoji()}`})       
         }
         let channel = interaction.options.getChannel("channel")
-        await channel.send({embeds:[msg]})
-        await interaction.reply("message envoyé")
+        let message = await channel.send({embeds:[msg]})
 
-        let emoji = interaction.options.getString('emoji')
-        message.react(emoji)
         const collector = message.createReactionCollector({dispose: true})
 
-        collector.on('collect', (reaction, user) =>{
-            const role = message.guild.roles.cache.find(reaction => reaction.name === "Il est la... toujours la...")
-            interaction.guild.members.cache.get(user.id).roles.add(role)
-            interaction.channel.send(`Vous avez maitenant le role ${role} ${user.tag}`)
-        });
-
-        collector.on('remove', (reaction, user) => {
-            const pasderoles = message.guild.roles.cache.find(r => r.name === "Il est la... toujours la...")
-            interaction.guild.members.cache.get(user.id).roles.remove(pasderoles)
-            interaction.channel.send(`${user.tag} n'a plus le role ${pasderoles}`)
-        });
+        for (let i = 0; i < reaction.recuperer_taille(); i++) {
+            let emoji = reaction.recuperer_reaction(i)
+            message.react(emoji.get_emoji())
+            console.log(emoji.get_emoji())
+            
+            collector.on('collect', (reaction, user) =>{
+                const role = message.guild.roles.cache.find(reaction => reaction.name === emoji.get_role().name)
+                interaction.guild.members.cache.get(user.id).roles.add(role)
+                if (reaction._emoji.name == emoji.get_emoji()){
+                    interaction.channel.send(`Vous avez maitenant le role ${role} ${user.tag}`)
+                }
+                console.log(reaction._emoji.name)
+            });
+    
+            collector.on('remove', (reaction, user) => {
+                const pasderoles = message.guild.roles.cache.find(r => r.name === emoji.get_role().name)
+                interaction.guild.members.cache.get(user.id).roles.remove(pasderoles)
+                if (reaction._emoji.name == emoji.get_emoji()){
+                    interaction.channel.send(`${user.tag} n'a plus le role ${pasderoles}`)
+                }
+            });
+        }
+        await interaction.reply("message envoyé")
     }
 }
